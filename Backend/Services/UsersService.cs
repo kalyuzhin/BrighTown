dc17912b17426f5dc6cfcc9259fc8b62d0c66a1d
kalyuzhin <67906018+kalyuzhin@users.noreply.github.com>
2023-11-27 21:58:43 +0300

    Implement register method

--- Backend/Services/UsersService.cs
+++ Backend/Services/UsersService.cs
@@ -2,4 +2,42 @@ namespace Backend.Services;
 
 public class UsersService : IUsersService
 {
+    private readonly IMapper _mapper;
+    private readonly DataContext _dataContext;
+
+    UsersService(DataContext dataContext, IMapper mapper)
+    {
+        _mapper = mapper;
+        _dataContext = dataContext;
+    }
+
+    public async Task<ServiceResponse<GetUserDto>> Register(AddUserDto newUser)
+    {
+        var serviceResponse = new ServiceResponse<GetUserDto>();
+        var db = _dataContext.Users;
+        if (db.ToList().Select(c => c.Email).Contains(newUser.Email))
+        {
+            serviceResponse.Success = false;
+            serviceResponse.Message = "This email address is already taken!";
+            return serviceResponse;
+        }
+
+        if (db.ToList().Select(c => c.Username).Contains(newUser.Username))
+        {
+            serviceResponse.Success = false;
+            serviceResponse.Message = "This username is already taken!";
+            return serviceResponse;
+        }
+
+        var user = _mapper.Map<User>(newUser);
+        await db.AddAsync(user);
+        await _dataContext.SaveChangesAsync();
+        serviceResponse.Data = _mapper.Map<GetUserDto>(user);
+        return serviceResponse;
+    }
+
+    // public async Task<ServiceResponse<GetUserDto>> AddFriend(GetUserDto user)
+    // {
+    //     
+    // }
 }

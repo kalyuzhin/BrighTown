5c239403ac1324c2e452eecdd675a1651ea4a661
kalyuzhin <67906018+kalyuzhin@users.noreply.github.com>
2023-11-27 16:56:12 +0300

    Update User Dtos and User model

--- Backend/Dtos/User/GetUserDto.cs
+++ Backend/Dtos/User/GetUserDto.cs
@@ -2,5 +2,10 @@ namespace Backend.Dtos.User;
 
 public class GetUserDto
 {
-    
+    public int Id { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public List<Models.Place> FavouritePlaces { get; set; } = new List<Models.Place>();
+    public List<Models.User> Friends { get; set; } = new List<Models.User>();
 }
--- Backend/Models/User.cs
+++ Backend/Models/User.cs
@@ -4,17 +4,17 @@ public class User
 {
     public int Id { get; set; }
 
-    // public string Email { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
 
     public string Username { get; set; } = string.Empty;
 
     public string Password { get; set; } = string.Empty;
 
-    // public string FirstName { get; set; } = string.Empty;
-    //
-    // public string SecondName { get; set; } = string.Empty;
-    //
-    // public List<Place> FavouritePlace { get; set; } = new();
-    //
-    // public List<User> Friends { get; set; } = new();
+    public string FirstName { get; set; } = string.Empty;
+    
+    public string SecondName { get; set; } = string.Empty;
+    
+    public List<Place> FavouritePlaces { get; set; } = new List<Place>();
+    
+    public List<User> Friends { get; set; } = new List<User>();
 }

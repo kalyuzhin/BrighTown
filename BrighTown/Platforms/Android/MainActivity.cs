3e450d921668fff5cddca1586d57cf277fa8271d
kalyuzhin <67906018+kalyuzhin@users.noreply.github.com>
2024-02-23 18:32:59 +0300

    Quick fix

--- BrighTown/Platforms/Android/MainActivity.cs
+++ BrighTown/Platforms/Android/MainActivity.cs
@@ -8,12 +8,4 @@ namespace BrighTown;
                            ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ScreenOrientation = ScreenOrientation.Portrait,
-        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
-    public class MainActivity : MauiAppCompatActivity
-    {
-      
-    }
-    
-    
 }

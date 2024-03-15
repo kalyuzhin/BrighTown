a0941d10595e1086c307874a7041e1966d95194c
kalyuzhin <67906018+kalyuzhin@users.noreply.github.com>
2024-03-15 15:23:36 +0300

    Add RefreshView on FavouritesPage

--- BrighTown/Pages/FavouritesPage.xaml.cs
+++ BrighTown/Pages/FavouritesPage.xaml.cs
@@ -1,4 +1,8 @@
+using System.Net.Http.Json;
+using System.Text;
 using BrighTown.Models;
+using CommunityToolkit.Maui.Core.Extensions;
+using Newtonsoft.Json;
 
 namespace BrighTown.Pages;
 
@@ -27,6 +31,33 @@ public partial class FavouritesPage : ContentPage
             FavouritesCollections.SelectedItem = null;
             // Routing.RegisterRoute("TakeALookOnPlace", typeof(PlaceDetails));
             Shell.Current.GoToAsync("PlaceDetails", navigation);
+        }
+    }
+
+    private async void RefreshView_OnRefreshing(object sender, EventArgs e)
+    {
+        using (HttpClient httpClient = new HttpClient())
+        {
+            string baseUrl = DeviceInfo.Platform == DevicePlatform.Android
+                ? "http://10.0.2.2:5280/"
+                : "http://localhost:5280/";
+            var url = baseUrl + "api/Places/getfavourites?id=";
+            var requestData = new Dictionary<string, int>()
+            {
+                { "id", App.user.Id }
+            };
+            var content = new
+                StringContent(JsonConvert.SerializeObject(requestData), Encoding.UTF8, "application/json");
+            url += App.user.Id;
+            var response = await httpClient.PostAsync(url, content);
+            var responseContent = await response.Content.ReadFromJsonAsync<ServiceResponse<List<Place2>>>();
+            if (responseContent.Data.Count == 0)
+            {
+                EmptyViewLabel.Text = responseContent.Message;
+            }
+
+            FavouritesCollections.ItemsSource = responseContent.Data.ToObservableCollection();
+            RefreshView.IsRefreshing = false;
         }
     }
 }
